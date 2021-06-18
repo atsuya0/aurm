8910ad8f70fe0a7c1fbb5ed081fa3d5aefe32dcd
tayusa <37957375+tayusa@users.noreply.github.com>
2021-06-18 17:56:09 +0900

    Move between files

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 const (
@@ -13,6 +15,24 @@ const (
 
 type pkgDownloader struct {
 	pkgNames []string
+}
+
+func getForeignPkgNames() ([]string, error) {
+	cmd := exec.Command("pacman", "-Qmq")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return make([]string, 0), fmt.Errorf("%w", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return make([]string, 0), fmt.Errorf("%w", err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stdout)
+	if err := cmd.Wait(); err != nil {
+		return make([]string, 0), fmt.Errorf("%w", err)
+	}
+	return strings.Split(strings.TrimSpace(buf.String()), "\n"), nil
 }
 
 func newPkgDownloader() (pkgDownloader, error) {
