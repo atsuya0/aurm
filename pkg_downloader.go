7f597d68b800a7cdecae3fd500043b1fa491ed9f
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2021-09-13 17:21:08 +0900

    Wrap errors

--- pkg_downloader.go
+++ pkg_downloader.go
@@ -22,19 +22,21 @@ type pkgDownloader struct {
 }
 
 func getForeignPkgNames() ([]string, error) {
+	errMsg := "Failed to get the foreign package names: %w"
+
 	cmd := exec.Command("pacman", "-Qmq")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return make([]string, 0), fmt.Errorf("%w", err)
+		return make([]string, 0), fmt.Errorf(errMsg, err)
 	}
 
 	if err := cmd.Start(); err != nil {
-		return make([]string, 0), fmt.Errorf("%w", err)
+		return make([]string, 0), fmt.Errorf(errMsg, err)
 	}
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(stdout)
 	if err := cmd.Wait(); err != nil {
-		return make([]string, 0), fmt.Errorf("%w", err)
+		return make([]string, 0), fmt.Errorf(errMsg, err)
 	}
 	return strings.Split(strings.TrimSpace(buf.String()), "\n"), nil
 }
@@ -42,32 +44,34 @@ func getForeignPkgNames() ([]string, error) {
 func newPkgDownloader() (pkgDownloader, error) {
 	pkgNames, err := getForeignPkgNames()
 	if err != nil {
-		return pkgDownloader{}, err
+		return pkgDownloader{}, fmt.Errorf("Failed to build the package downloader: %w", err)
 	}
 
 	return pkgDownloader{pkgNames: pkgNames}, nil
 }
 
 func (p *pkgDownloader) fetchPkgIfNeeded() error {
+	errMsg := "Failed to fetch the package: %w"
+
 	for _, pkgName := range p.pkgNames {
 		localVer, err := p.getLocalVer(pkgName)
 		exitError := &exec.ExitError{}
 		if errors.As(err, &exitError) {
 			if err := p.fetchPkg(pkgName); err != nil {
-				return err
+				return fmt.Errorf(errMsg, err)
 			}
 			continue
 		} else if err != nil {
-			return err
+			return fmt.Errorf(errMsg, err)
 		}
 		remoteVer, err := p.fetchRemoteVer(pkgName)
 		if err != nil {
-			return err
+			return fmt.Errorf(errMsg, err)
 		}
 		if localVer != remoteVer {
 			fmt.Printf("Download %s\n", pkgName)
 			if err := p.fetchPkg(pkgName); err != nil {
-				return err
+				return fmt.Errorf(errMsg, err)
 			}
 		}
 	}
@@ -75,15 +79,17 @@ func (p *pkgDownloader) fetchPkgIfNeeded() error {
 }
 
 func (p *pkgDownloader) getLocalVer(pkgName string) (string, error) {
+	errMsg := "Failed to get the package local version"
+
 	cmd := exec.Command("pacman", "-Qi", pkgName)
 	os.Setenv("LANG", "C")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return "", fmt.Errorf("%w", err)
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
 
 	if err := cmd.Start(); err != nil {
-		return "", fmt.Errorf("%w", err)
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
 	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
@@ -91,15 +97,15 @@ func (p *pkgDownloader) getLocalVer(pkgName string) (string, error) {
 		if strings.HasPrefix(text, "Version") {
 			ver := strings.TrimSpace(text[strings.Index(text, ":")+1:])
 			if err := cmd.Wait(); err != nil {
-				return "", fmt.Errorf("%w", err)
+				return "", fmt.Errorf(errMsg+": %w", err)
 			}
 			return ver, nil
 		}
 	}
 	if err := cmd.Wait(); err != nil {
-		return "", fmt.Errorf("%w", err)
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
-	return "", errors.New("Cannot get package local version.")
+	return "", errors.New(errMsg)
 }
 
 type pkg struct {
@@ -109,9 +115,11 @@ type pkg struct {
 }
 
 func (p *pkgDownloader) fetchRemoteVer(pkgName string) (string, error) {
+	errMsg := "Failed to fetch the package remote version"
+
 	res, err := http.Get(aurHost + "/rpc/?v=5&type=info&arg[]=" + pkgName)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
 	defer func() {
 		if err = res.Body.Close(); err != nil {
@@ -121,24 +129,26 @@ func (p *pkgDownloader) fetchRemoteVer(pkgName string) (string, error) {
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
 	pkg := pkg{}
 	err = json.Unmarshal(body, &pkg)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(errMsg+": %w", err)
 	}
 	if len(pkg.Results) < 1 {
-		return "", errors.New("Cannot fetch the remote package version")
+		return "", errors.New(errMsg)
 	}
 	return pkg.Results[0].Version, nil
 }
 
 func (p *pkgDownloader) fetchPkg(pkgName string) error {
+	errMsg := "Failed to fetch the package: %w"
+
 	fileName := pkgName + ".tar.gz"
 	res, err := http.Get(aurHost + path.Join("/cgit/aur.git/snapshot", fileName))
 	if err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf(errMsg, err)
 	}
 	defer func() {
 		if err = res.Body.Close(); err != nil {
@@ -146,12 +156,12 @@ func (p *pkgDownloader) fetchPkg(pkgName string) error {
 		}
 	}()
 	if res.StatusCode == http.StatusNotFound {
-		return errors.New(res.Status)
+		return fmt.Errorf("%w", errors.New(res.Status))
 	}
 
 	gzipReader, err := gzip.NewReader(res.Body)
 	if err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf(errMsg, err)
 	}
 	defer func() {
 		if err = gzipReader.Close(); err != nil {
@@ -165,17 +175,17 @@ func (p *pkgDownloader) fetchPkg(pkgName string) error {
 		if errors.Is(err, io.EOF) {
 			break
 		} else if err != nil {
-			return fmt.Errorf("%w", err)
+			return fmt.Errorf(errMsg, err)
 		}
 		switch header.Typeflag {
 		case tar.TypeDir:
 			if err := os.Mkdir(header.Name, 0700); err != nil {
-				return fmt.Errorf("%w", err)
+				return fmt.Errorf(errMsg, err)
 			}
 		case tar.TypeReg:
 			newFile, err := os.Create(header.Name)
 			if err != nil {
-				return fmt.Errorf("%w", err)
+				return fmt.Errorf(errMsg, err)
 			}
 			defer func() {
 				if err = newFile.Close(); err != nil {
@@ -183,7 +193,7 @@ func (p *pkgDownloader) fetchPkg(pkgName string) error {
 				}
 			}()
 			if _, err := io.Copy(newFile, tarReader); err != nil {
-				return fmt.Errorf("%w", err)
+				return fmt.Errorf(errMsg, err)
 			}
 		}
 	}
